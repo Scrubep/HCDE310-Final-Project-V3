89a49161bd3737055837bffc6ebab324c2cd34ca
Joey Choi <schoi34@uw.edu>
2024-12-01 01:07:07 -0800

    Did some work for html and functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import function
-import requests
+from flask import Flask, render_template, request, session
+from function import find_recommendations_steam, create_steam_data, sort_playtimes, individual_game_data
 
 # The app will start at index.html
 
@@ -13,3 +13,36 @@ import requests
 # summary of the game within it. I'll need to figure out how to link the different
 # games to the different links.
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    if request.method == 'POST':
+        steam_id = request.form['steam_id']
+        data = create_steam_data(steam_id)
+        playtimes = sort_playtimes(data)
+
+        counter = 0
+        top_playtimes = {}
+        for game in playtimes:
+            if (counter < 5):
+                top_playtimes[game] = playtimes[game]
+                counter += 1
+            else:
+                break
+
+        list = find_recommendations_steam(steam_id, int(request.form['input_page']))
+
+        return render_template('results.html', counter=0, playtimes=top_playtimes, game_list=list)
+
+@app.route('/game', methods=['GET', 'POST'])
+def game():
+    if request.method == 'POST':
+        input_game_slug = request.form['input_game']
+        input_game = individual_game_data(input_game_slug)
+
+        return render_template('game.html', input_game=input_game)
--- function.py
+++ function.py
@@ -1,13 +1,12 @@
+import string
 import urllib.parse, urllib.request, urllib.error, json
 import collections
 import keys
-from Game import Game
 
 # Creates json data. Is used for both RAWG and steam data below.
 def create_json_data(base_url, parameters):
     param_str = urllib.parse.urlencode(parameters)
     request_url = base_url + "?" + param_str
-    # print(request_url)
     f = urllib.request.urlopen(request_url)
     json_url = ''
     for line in f:
@@ -15,26 +14,44 @@ def create_json_data(base_url, parameters):
     json_data = json.loads(json_url)
     return json_data
 
+def strip_word_punctuation(word):
+    # Note - this function is far from comprehensive, and so some punctuation may
+    # still make it through
+    word = word.replace("&lt;","<").replace("&gt;",">").replace("&amp;","&")
+    return word.strip("&=.,()<>\"\\'~?!;*:[]-+_/`\u2014\u2018\u2019\u201c\u201d\u200b").replace("\"","\"\"")
+
+# Converts name into something that is readable by RAWG.
 def name_convert(name):
-    name = name.lower().replace(" ", "-").replace(":", "")
+    name = name.lower().replace(" ", "-").replace(":", "").replace("'", "")
     parenth_index = name.find("(")
     if parenth_index > -1:
         name = name[0:parenth_index - 1]
     return name
 
+# Finds the info of individual games using RAWG.
 def individual_game_data(name):
     if " " in name:
-        name = name_convert(name)
+        name = strip_word_punctuation(name_convert(name))
     base_url = "https://api.rawg.io/api/games/" + name
     return create_json_data(base_url, {"key": keys.RAWG_key})
 
 # Is used to find data based on a person's steam account.
 # This specifically just gives the game name and hours played by the user.
 def create_steam_data(steam_id):
-    # Finds all games that the user owns
+    # Checks to see if the steam_id is valid
+    steam_id.replace(" ", "")
+    steam_id.translate(str.maketrans('', '', string.punctuation))
+    if len(steam_id) != 17 or any(c.isalpha() for c in steam_id):
+        return None
+
     base_url = " http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001"
     parameters = {"key": keys.steam_key, "steamid": steam_id, "include_appinfo": "True",
                   "include_played_free_games": "True"}
+    try:
+        create_json_data(base_url, parameters)
+    except urllib.error.HTTPError:
+        return None
+
     return create_json_data(base_url, parameters)
 
 
@@ -49,21 +66,19 @@ def sort_playtimes(owned_games):
     sorted_playtimes = sorted(playtimes.items(), key=lambda item: item[1], reverse=True)
     final_playtimes = collections.OrderedDict(sorted_playtimes)
 
-    # Prints out top 5 games (will delete later, just for testing)
-    counter = 0;
-    for game in final_playtimes:
-        if (counter < 5):
-            print(game + ": " + str(final_playtimes[game]))
-            counter += 1
-        else:
-            break
     return final_playtimes
 
 # Gets the most played genres of the user based on their user statistics.
-# Based on their 3 most played games (subject to change).
+# Based on their 5 most played games (subject to change).
 def most_played_genres(steam_id):
     genres_list = []
-    steam_data = sort_playtimes(create_steam_data(steam_id))
+    data = create_steam_data(steam_id)
+
+    # Checks to see if the steam_id is valid
+    if data is None or len(data["response"]) == 0:
+        return genres_list
+
+    steam_data = sort_playtimes(data)
     counter = 0;
     for game in steam_data:
         if counter < 5:
@@ -92,7 +107,6 @@ def most_played_genres(steam_id):
     for genre in genres_list:
         genres += "," + genre
 
-    print(genres) # Test to see what genres are passed in to parameters.
     return genres
 
 # Recommendations based on a person's steam data. Returns a list of games that are rated
@@ -100,6 +114,11 @@ def most_played_genres(steam_id):
 # have its inventory set to public.
 def find_recommendations_steam(steam_id, page_size=10):
     genres = most_played_genres(steam_id)
+
+    # Checks to see if the steam_id is valid
+    if len(genres) == 0:
+        return []
+
     base_url = "https://api.rawg.io/api/games"
     parameters = {"key": keys.RAWG_key, "page_size": page_size, "genres": genres.lower(), "ordering": "metacritic-"}
     RAWG_data = create_json_data(base_url, parameters)
@@ -107,8 +126,7 @@ def find_recommendations_steam(steam_id, page_size=10):
     game_list = []
     for game in RAWG_data["results"]:
         game_info = individual_game_data(game["slug"])
-        description = game_info["description"]
-        game_list.append(Game(game["name"], game["genres"], game["background_image"], description))
+        game_list.append(game_info)
 
     return game_list
 
@@ -123,7 +141,7 @@ def find_recommendations_steam(steam_id, page_size=10):
 # list = find_recommendations_steam("76561199105520418") # Anonymous K
 # list = find_recommendations_steam("76561199388773489") # Anonymous J
 # list = find_recommendations_steam("76561199202887189") # Anonymous L
-list = find_recommendations_steam("76561197960434622") # Random dude
-print("----------------------------")
-for game in list:
-    print(game.title)
+# list = find_recommendations_steam("76561197960434622") # Random dude
+# print("----------------------------")
+# for game in list:
+#     print(game.title)
